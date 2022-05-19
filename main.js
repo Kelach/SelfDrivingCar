345278c8e96ccd7d83d2e5c7b6f99dadc8d4f698
Kelach <93161153+Kelach@users.noreply.github.com>
2022-05-18 23:11:23 -0500

    add car.js file. building html, and physics behind car mechanics

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
-'use strict';
+const canvas = document.getElementById("myCanvas");
+canvas.height = window.innerHeight
+canvas.width = 200;
 
-console.log('Hello world');
+const ctx = canvas.Getcontext("2d");
+const car = new Car(100, 100, 30, 50)
+car.draw(ctx)
